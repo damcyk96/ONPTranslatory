23c49857ec6a5d130451df4ed0fd5f72096112c3
Damian Obajtek <damian.obajtek@gmail.com>
2020-09-17 12:23:21 +0200

    zaliczenie

---
+++ ONPLogic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ONP
+{
+    public class ONPLogic
+    {
+        List<string> listOfEwerything;
+        List<double> listOfNumbers;
+        public ONPLogic(string equation)
+        {
+            listOfEwerything = new List<string>();
+            listOfNumbers = new List<double>();
+            AddToList(equation);
+            NubmersConverter();
+        }
+        private void NubmersConverter()
+        {
+            for(int i = 0; i < listOfEwerything.Count(); i++)
+                if (listOfEwerything[i] != "+" && listOfEwerything[i] != "-" && listOfEwerything[i] != "*" &&
+                    listOfEwerything[i] != "/" && listOfEwerything[i] != "^")
+                {
+                    double temporary = double.Parse(listOfEwerything[i]);
+                    listOfNumbers.Add(temporary);
+                } 
+        }
+        private void AddToList(string str)
+        {
+            string[] elements = str.Split(' ');
+            foreach(string element in elements)
+                listOfEwerything.Add(element);
+        }
+        public double Calculator()
+        {
+            for (int i = 0; i < listOfEwerything.Count(); i++)
+            { 
+                if (listOfEwerything[i] == "+")
+                {
+                    listOfEwerything.RemoveAt(i);
+                    listOfNumbers[i - 2] += listOfNumbers[i - 1];
+                    listOfNumbers.RemoveAt(i - 1);
+                    listOfEwerything.RemoveAt(i - 1);
+                    i = 0;
+                }
+                else if(listOfEwerything[i] == "-")
+                {
+                    listOfEwerything.RemoveAt(i);
+                    listOfNumbers[i - 2] -= listOfNumbers[i - 1];
+                    listOfNumbers.RemoveAt(i - 1);
+                    listOfEwerything.RemoveAt(i - 1);
+                    i = 0;
+                }
+                else if (listOfEwerything[i] == "*")
+                {
+                    listOfEwerything.RemoveAt(i);
+                    listOfNumbers[i - 2] *= listOfNumbers[i - 1];
+                    listOfNumbers.RemoveAt(i - 1);
+                    listOfEwerything.RemoveAt(i - 1);
+                    i = 0;
+                }
+                else if (listOfEwerything[i] == "/")
+                {
+                    listOfEwerything.RemoveAt(i);
+                    listOfNumbers[i - 2] /= listOfNumbers[i - 1];
+                    listOfNumbers.RemoveAt(i - 1);
+                    listOfEwerything.RemoveAt(i - 1);
+                    i = 0;
+                }
+                else if (listOfEwerything[i] == "^")
+                {
+                    listOfEwerything.RemoveAt(i);
+                    listOfNumbers[i - 2] = Math.Pow(listOfNumbers[i - 2] , listOfNumbers[i - 1]);
+                    listOfNumbers.RemoveAt(i - 1);
+                    listOfEwerything.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+            return listOfNumbers[0];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ONP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCalculation = true;
+            while (newCalculation)
+            {
+                Console.WriteLine("Wprowadź cyfry i znaki oddzielone spacją: ");
+                try
+                {
+                    string equation = Console.ReadLine();
+                    ONPLogic onpLogic = new ONPLogic(equation);
+                    Console.WriteLine(onpLogic.Calculator());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Tylko liczby i + - * / ^ te znaki.");
+                }
+                Console.WriteLine("Chcesz wprowadzić nowe obliczenie?(y/n)");
+                if (Console.ReadLine() != "y")
+                    newCalculation = false;
+            }
+            Console.ReadKey();
+        }  
+    }
+
+}
